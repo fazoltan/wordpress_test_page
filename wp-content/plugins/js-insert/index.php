47c596f2a0d3d2a9cf98c05104251905baa0780c
fazoltan <fazoltan@gmail.com>
2020-06-24 13:32:04 +0200

    Feat: short code is working

--- wp-content/plugins/js-insert/index.php
+++ wp-content/plugins/js-insert/index.php
@@ -36,20 +36,74 @@ function add_my_scripts(){
             'actionName' => 'it_ajax'
         ]);
 
+    //css oldal hozzáadása
+    wp_enqueue_style( 
+            'page-style',//string $handle, sítuslap nevének a megadása 
+            plugins_url('js-insert/css/page-css.css'),//string $src = '',az elérési út megadása 
+            array(),//string[] $deps = array(), függőségek megadása 
+            date('YmdHis')//string|bool|null $ver = false, verzió megadása
+            //string $media = 'all' ) Melyik médiára legyen alkalmazva pl.: print. alap értelmezette all
+    );
+
 }
 
 add_action( 'wp_enqueue_scripts', 'add_my_scripts' );
 
 
-function add_my_styles(){
-    wp_enqueue_style( 
-        'page-style',//string $handle, sítuslap nevének a megadása 
-        plugins_url('js-insert/css/page-css.css'),//string $src = '',az elérési út megadása 
-        array(),//string[] $deps = array(), függőségek megadása 
-        date('YmdHis')//string|bool|null $ver = false, verzió megadása
-        //string $media = 'all' ) Melyik médiára legyen alkalmazva pl.: print. alap értelmezette all
-    );
-}
-add_action( 'wp_enqueue_scripts', 'add_my_styles' );
 
+
+// Shortcode hozzáadása a wordpress rendszerhez
+
+//három artibutumot ad vissza: 1) attributumok 2) tartalom 3) A shorthcode neve , return json_encode(func_get_args());
+function add_subsribe( $atts, $content , $name){
+    //html-el dolgozunk, ezért egy ob buffert inditunk, hogy ne kerüljenek ki a dolgok élesben
+    ob_start(); //ami ezután van az a php el fogja rejteni és egy pufferben fogja tárolni
+    ?>
+
+    <div class="urgent <?php echo $atts['cssclass']; /* Így egyedi sosztájt is tudunk hozzadni*/?>"> 
+        <?php echo $content; ?>
+    </div>
+    <?php
+
+    $content = ob_get_contents(); //a buffer tartalmat kiolvassuk
+    ob_clean(); //a buffert kiüritjük
+
+    return $content; //azért érdemes a visszatérési érétket használni, mert a megfelelő helyen fog mejlennni a tartalom
+    
+}
+
+add_shortcode('subsribe','add_subsribe');
+
+/*function add_subscibe_form($atts){
+    return json_encode($atts);
+}
+
+add_shortcode('subscripbe-form','add_subscibe_form');//1param: a short code neve, 2param: a függvény neve ami lekezeli
+
+
+function wpdocs_bartag_func( $atts ) {
+    $atts = shortcode_atts( array(
+        'foo' => 'no foo',
+        'baz' => 'default baz'
+    ), $atts, 'bartag' );
+ 
+    return "foo = {$atts['foo']}";
+}
+
+add_shortcode( 'bartag', 'wpdocs_bartag_func' );
+*/
+
+/*function 'init_func'(){
+    function wpdocs_bartag_func( $atts ) {
+    $atts = shortcode_atts( array(
+        'foo' => 'no foo',
+        'baz' => 'default baz'
+    ), $atts, 'bartag' );
+ 
+    return "foo = {$atts['foo']}";
+}
+
+add_shortcode( 'bartag', 'wpdocs_bartag_func' );
+}
+add_action('init','init_func');*/
 
